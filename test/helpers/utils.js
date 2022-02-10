083089e777e3ff44401392fb86b14ad8ace78111
Ilia Sidorenko <ilia.sidorenko.421@gmail.com>
2022-02-10 23:02:48 +1300

    add helpers file

---
+++ test/helpers/utils.js
@@ -0,0 +1,45 @@
+
+function buffer2bitArray (b) {
+    const res = [];
+    for (let i=0; i<b.length; i++) {
+        for (let j=0; j<8; j++) {
+            res.push((b[i] >> (7-j) &1));
+        }
+    }
+    return res;
+}
+
+function bitArray2buffer (a) {
+    const len = Math.floor((a.length -1 )/8)+1;
+    const b = new Buffer.alloc(len);
+
+    for (let i=0; i<a.length; i++) {
+        const p = Math.floor(i/8);
+        b[p] = b[p] | (Number(a[i]) << ( 7 - (i%8)  ));
+    }
+    return b;
+}
+
+function arrayChunk (array, chunk_size) {
+    return Array(Math.ceil(array.length / chunk_size)).fill().map((_, index) => index * chunk_size).map(begin => array.slice(begin, begin + chunk_size));
+}
+
+function padMessage(bits) {
+    const L = bits.length;
+    const K = (512 + 448 - (L % 512 + 1)) % 512;
+
+    bits = bits.concat([1]);
+    if(K > 0) {
+        bits = bits.concat(Array(K).fill(0));
+    }
+    bits = bits.concat(buffer2bitArray(Buffer.from(L.toString(16).padStart(16, '0'), 'hex')))
+    
+    return bits;
+}
+
+module.exports = {
+    buffer2bitArray,
+    bitArray2buffer,
+    arrayChunk,
+    padMessage,
+}
--- test/nzcp.js
+++ test/nzcp.js
@@ -14,27 +14,8 @@ const wasm_tester = require("circom_tester").wasm;
 
 // const printSignal = require("./helpers/printsignal");
 
+const {buffer2bitArray, bitArray2buffer} = require("./helpers/utils");
 
-function buffer2bitArray(b) {
-    const res = [];
-    for (let i=0; i<b.length; i++) {
-        for (let j=0; j<8; j++) {
-            res.push((b[i] >> (7-j) &1));
-        }
-    }
-    return res;
-}
-
-function bitArray2buffer(a) {
-    const len = Math.floor((a.length -1 )/8)+1;
-    const b = new Buffer.alloc(len);
-
-    for (let i=0; i<a.length; i++) {
-        const p = Math.floor(i/8);
-        b[p] = b[p] | (Number(a[i]) << ( 7 - (i%8)  ));
-    }
-    return b;
-}
 
 
 describe("SHA256", function () {
@@ -188,3 +169,4 @@ describe("NZCP", function () {
         assert.equal(hash2, "5fb355822221720ea4ce6734e5a09e459d452574a19310c0cea7c141f43a3dab");
     });
 });
+
