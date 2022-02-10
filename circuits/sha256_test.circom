88e399fb5a20bbadd11ce8c869960f5fcbf98489
Ilia Sidorenko <ilia.sidorenko.421@gmail.com>
2022-02-10 23:03:02 +1300

    add sha256 test

---
+++ circuits/sha256_test.circom
@@ -0,0 +1,19 @@
+pragma circom 2.0.0;
+
+include "../snark-jwt-verify-master/circuits/sha256.circom";
+
+template Sha256Test() {
+    signal input in[512];
+    signal output out[256];
+
+    component sha256_unsafe = Sha256_unsafe(1);
+    for (var i=0; i<512; i++) {
+        sha256_unsafe.in[0][i] <== in[i];
+    }
+    sha256_unsafe.tBlock <== 1;
+
+    for (var i=0; i<256; i++) {
+        out[i] <== sha256_unsafe.out[i];
+    }
+}
+component main = Sha256Test();
---
+++ test/sha256.js
@@ -0,0 +1,41 @@
+const chai = require("chai");
+const path = require("path");
+const wasm_tester = require("circom_tester").wasm;
+const {buffer2bitArray, bitArray2buffer, arrayChunk, padMessage} = require("./helpers/utils");
+const assert = chai.assert;
+
+function genSha256Inputs(input, nCount, nWidth = 512, inParam = "in") {
+    var segments = arrayChunk(padMessage(buffer2bitArray(Buffer.from(input))), nWidth);
+    const tBlock = segments.length / (512 / nWidth);
+    
+    if(segments.length < nCount) {
+        segments = segments.concat(Array(nCount-segments.length).fill(Array(nWidth).fill(0)));
+    }
+    
+    if(segments.length > nCount) {
+        throw new Error('Padded message exceeds maximum blocks supported by circuit');
+    }
+    
+    return { segments, "tBlock": tBlock }; 
+}
+
+
+describe("Sha256", function () {
+    this.timeout(100000);
+    it ("Should parse ToBeSigned", async () => {
+        const p = path.join(__dirname, "../", "circuits", "sha256_test.circom")
+        const cir = await wasm_tester(p);
+
+        const input = genSha256Inputs("Jack,Sparrow,1960-04-16", 1);
+
+        console.log('calculating witness...');
+
+        console.log('tBlock',input.tBlock)
+        const witness = await cir.calculateWitness({ "in": input.segments[0] }, true);
+
+        const arrOut = witness.slice(1, 257);
+        const hash2 = bitArray2buffer(arrOut).toString("hex");
+
+        assert.equal(hash2,"5fb355822221720ea4ce6734e5a09e459d452574a19310c0cea7c141f43a3dab")
+    });
+});
