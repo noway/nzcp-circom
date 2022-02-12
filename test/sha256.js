a3c65a542e9765acb8b868de0ed90a499abf52f0
Ilia Sidorenko <ilia.sidorenko.421@gmail.com>
2022-02-12 17:52:42 +1300

    add simple helper functions for 1/2 block messages

--- test/sha256.js
+++ test/sha256.js
@@ -20,6 +20,19 @@ function genSha256Inputs(input, nCount, nWidth = 512, inParam = "in") {
     return { segments, "tBlock": tBlock }; 
 }
 
+function msgToBitsAs1Block(msg) {
+    let inn = buffer2bitArray(Buffer.from(msg))
+    const add_bits = 512 - inn.length
+    inn = inn.concat(Array(add_bits).fill(0));
+    return inn
+}
+
+function msgToBitsAs2Blocks(msg) {
+    let inn = buffer2bitArray(Buffer.from(msg))
+    const add_bits = 1024 - inn.length
+    inn = inn.concat(Array(add_bits).fill(0));
+    return inn
+}
 
 describe("Sha256", function () {
     this.timeout(100000);
@@ -54,9 +67,10 @@ describe("Sha256", function () {
             const len = message.length;
             console.log("message", message, len)
 
-            let inn = buffer2bitArray(Buffer.from(message))
-            const add_bits = 512-inn.length
-            inn = inn.concat(Array(add_bits).fill(0));
+            // let inn = buffer2bitArray(Buffer.from(message))
+            // const add_bits = 512-inn.length
+            // inn = inn.concat(Array(add_bits).fill(0));
+            const inn = msgToBitsAs1Block(message)
 
             const witness = await cir.calculateWitness({ "in": inn, len }, true);
 
@@ -75,12 +89,13 @@ describe("Sha256", function () {
         const cir = await wasm_tester(p);
 
         const message = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
-        const input = genSha256Inputs(message, 2);
-        console.log('input.segments[0]',bitArray2buffer(input.segments[0]).toString('hex'))
-        console.log('input.segments[1]',bitArray2buffer(input.segments[1]).toString('hex'))
+        // const input = genSha256Inputs(message, 2);
+        // console.log('input.segments[0]',bitArray2buffer(input.segments[0]).toString('hex'))
+        // console.log('input.segments[1]',bitArray2buffer(input.segments[1]).toString('hex'))
         const len = message.length;
+        const inn = msgToBitsAs2Blocks(message)
         
-        const witness = await cir.calculateWitness({ "in": input.segments[0].concat(input.segments[1]), len }, true);
+        const witness = await cir.calculateWitness({ "in": inn, len }, true);
 
         const arrOut = witness.slice(1, 257);
         const actualHash = bitArray2buffer(arrOut).toString("hex");
