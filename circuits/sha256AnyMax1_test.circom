53d45a5b899bcc6082103f5327ebbd05b40ee0ed
Ilia Sidorenko <ilia.sidorenko.421@gmail.com>
2022-02-13 00:10:49 +1300

    test all block spaces

---
+++ circuits/sha256AnyMax1_test.circom
@@ -0,0 +1,5 @@
+pragma circom 2.0.0;
+
+include "./sha256Any.circom";
+
+component main = Sha256Any(1);
--- test/sha256any.js
+++ test/sha256any.js
@@ -5,40 +5,54 @@ const {buffer2bitArray, bitArray2buffer, arrayChunk, padMessage} = require("./he
 const assert = chai.assert;
 const Sha256 = require('./helpers/sha256')
 
-function msgToBits(msg) {
+function msgToBits(msg, blocks) {
     let inn = buffer2bitArray(Buffer.from(msg))
-    // const blocks = Math.floor((inn.length + 64) / 512) + 1
-    const blocks = 4;
     const overall_len = blocks * 512
     const add_bits = overall_len - inn.length
     inn = inn.concat(Array(add_bits).fill(0));
     return inn
 }
 
+async function testBlockSpace(block_space) {
+    const blocks = Math.pow(2, block_space);
+    const max_len = (blocks * 512 - 64) / 8;
+    console.log('block_space', block_space, 'blocks', blocks, 'max_len', max_len);
+
+    const p = path.join(__dirname, `../circuits/sha256AnyMax${block_space}_test.circom`)
+    const cir = await wasm_tester(p);
+
+    for(let i = 0; i < max_len; i++) {
+        const message = Array(i).fill("a").join("")
+        const len = message.length * 8;
+        const inn = msgToBits(message, blocks)
+        console.log("message", message, len, len / 8)
+
+        const witness = await cir.calculateWitness({ "in": inn, len }, true);
+
+        const arrOut = witness.slice(1, 257);
+        const actualHash = bitArray2buffer(arrOut).toString("hex");
+        const expectedHash = Sha256.hash(message)
+        assert.equal(actualHash, expectedHash)
+    }
+}
+
 describe("Sha256", function () {
     this.timeout(1000000000);
 
-    // TODO: into a separate test
-    it ("Should generate hash for 1-4 blocks", async () => {
-        const p = path.join(__dirname, "../", "circuits", "sha256Any_test.circom")
-        const cir = await wasm_tester(p);
+    it ("Should generate hash for 1 block space", async () => {
+        await testBlockSpace(1);
+    });
 
-        for(let i=0; i<248; i++) {
+    it ("Should generate hash for 2 block space", async () => {
+        await testBlockSpace(2);
+    });
 
-            const message = Array(i).fill("a").join("")
-            const len = message.length * 8;
-            console.log("message", message, len, len / 8)
+    it ("Should generate hash for 3 block space", async () => {
+        await testBlockSpace(3);
+    });
 
-            const inn = msgToBits(message)
-
-            const witness = await cir.calculateWitness({ "in": inn, len }, true);
-
-            const arrOut = witness.slice(1, 257);
-            const hash2 = bitArray2buffer(arrOut).toString("hex");
-
-            assert.equal(hash2,Sha256.hash(message))
-        }
-
+    it ("Should generate hash for 4 block space", async () => {
+        await testBlockSpace(4);
     });
 
 
