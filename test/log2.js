5d6dae83f7b8df9eb2bf8697dea857940a097865
Ilia Sidorenko <ilia.sidorenko.421@gmail.com>
2022-02-20 19:30:23 +1300

    tidy up log2.js tests

--- test/log2.js
+++ test/log2.js
@@ -4,17 +4,12 @@ const assert = chai.assert;
 
 const wasm_tester = require("circom_tester").wasm;
 
-const {buffer2bitArray, bitArray2buffer} = require("./helpers/utils");
-
 
 describe("CBOR getType", function () {
     const p = path.join(__dirname, "../circuits/log2_test.circom")
     it ("log2 works", async () => {
         // exhaustive test for 8 bits
         const cir = await wasm_tester(p);
-        // for (let v = 255; v >= 0; v--) {
-            const witness = await cir.calculateWitness({  }, true);
-            // assert.equal(witness[1], v >> 5);
-        // }
+        const witness = await cir.calculateWitness({  }, true);
     });
 });
