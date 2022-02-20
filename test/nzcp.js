fa2556e3622410052861a1433e9770034bbc8d50
Ilia Sidorenko <ilia.sidorenko.421@gmail.com>
2022-02-20 22:43:02 +1300

    remove magic numbers

--- test/nzcp.js
+++ test/nzcp.js
@@ -22,6 +22,7 @@ function sha256Hash(input) {
 const EXAMPLE_PASS_URI = "NZCP:/1/2KCEVIQEIVVWK6JNGEASNICZAEP2KALYDZSGSZB2O5SWEOTOPJRXALTDN53GSZBRHEXGQZLBNR2GQLTOPICRUYMBTIFAIGTUKBAAUYTWMOSGQQDDN5XHIZLYOSBHQJTIOR2HA4Z2F4XXO53XFZ3TGLTPOJTS6MRQGE4C6Y3SMVSGK3TUNFQWY4ZPOYYXQKTIOR2HA4Z2F4XW46TDOAXGG33WNFSDCOJONBSWC3DUNAXG46RPMNXW45DFPB2HGL3WGFTXMZLSONUW63TFGEXDALRQMR2HS4DFQJ2FMZLSNFTGSYLCNRSUG4TFMRSW45DJMFWG6UDVMJWGSY2DN53GSZCQMFZXG4LDOJSWIZLOORUWC3CTOVRGUZLDOSRWSZ3JOZSW4TTBNVSWISTBMNVWUZTBNVUWY6KOMFWWKZ2TOBQXE4TPO5RWI33CNIYTSNRQFUYDILJRGYDVAYFE6VGU4MCDGK7DHLLYWHVPUS2YIDJOA6Y524TD3AZRM263WTY2BE4DPKIF27WKF3UDNNVSVWRDYIYVJ65IRJJJ6Z25M2DO4YZLBHWFQGVQR5ZLIWEQJOZTS3IQ7JTNCFDX";
 
 async function testNZCPCredSubjHash(cir, passURI, isLive, maxLen) {
+    const SHA256_BITS = 256;
     const verificationResult = verifyPassURIOffline(passURI, { didDocument: isLive ? DID_DOCUMENTS.MOH_LIVE : DID_DOCUMENTS.MOH_EXAMPLE })
     const credSubj = verificationResult.credentialSubject;
 
@@ -32,15 +33,15 @@ async function testNZCPCredSubjHash(cir, passURI, isLive, maxLen) {
     const witness = await cir.calculateWitness(input, true);
 
     const expectedCredSubjHash = sha256Hash(credSubjConcat)
-    const credSubjHash = bitArray2buffer(witness.slice(1, 257)).toString("hex");
+    const credSubjHash = bitArray2buffer(witness.slice(1, 1 + SHA256_BITS)).toString("hex");
     assert.equal(credSubjHash, expectedCredSubjHash);
 
     const expectedToBeSignedHash = sha256Hash(toBeSignedByteArray)
-    const toBeSignedHash = bitArray2buffer(witness.slice(257, 257+256)).toString("hex");
+    const toBeSignedHash = bitArray2buffer(witness.slice(1 + SHA256_BITS, 1 + 2 * SHA256_BITS)).toString("hex");
     assert.equal(toBeSignedHash, expectedToBeSignedHash);
 
     const expectedExp = verificationResult.raw.exp
-    const actualExp = witness[257+256]
+    const actualExp = witness[1 + 2 * SHA256_BITS]
     assert.equal(expectedExp, actualExp)
 }
 
