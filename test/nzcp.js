43372b1404e5f5a1096f07ff8a79de673e839e2d
Ilia Sidorenko <ilia.sidorenko.421@gmail.com>
2022-02-20 22:54:56 +1300

    add tests for 4 live passes (must be supplied via .env)

--- test/nzcp.js
+++ test/nzcp.js
@@ -6,6 +6,8 @@ const { verifyPassURIOffline, DID_DOCUMENTS } = require("@vaxxnz/nzcp");
 const { getToBeSignedAndRs } = require('./helpers/nzcp');
 const { buffer2bitArray, bitArray2buffer } = require("./helpers/utils");
 
+require('dotenv').config()
+
 const assert = chai.assert;
 
 
@@ -27,6 +29,7 @@ async function testNZCPCredSubjHash(cir, passURI, isLive, maxLen) {
     const credSubj = verificationResult.credentialSubject;
 
     const credSubjConcat = `${credSubj.givenName},${credSubj.familyName},${credSubj.dob}`
+    console.log('credSubjConcat', credSubjConcat);
     const toBeSignedByteArray = Buffer.from(getToBeSignedAndRs(passURI).ToBeSigned, "hex");
 
     const input = prepareNZCPCredSubjHashInput(toBeSignedByteArray, maxLen);
@@ -35,16 +38,19 @@ async function testNZCPCredSubjHash(cir, passURI, isLive, maxLen) {
     const expectedCredSubjHash = sha256Hash(credSubjConcat)
     const credSubjHash = bitArray2buffer(witness.slice(1, 1 + SHA256_BITS)).toString("hex");
     assert.equal(credSubjHash, expectedCredSubjHash);
+    console.log('credSubjHash', credSubjHash);
 
     const expectedToBeSignedHash = sha256Hash(toBeSignedByteArray)
     const toBeSignedHash = bitArray2buffer(witness.slice(1 + SHA256_BITS, 1 + 2 * SHA256_BITS)).toString("hex");
     assert.equal(toBeSignedHash, expectedToBeSignedHash);
+    console.log('toBeSignedHash', toBeSignedHash);
 
     const expectedExp = verificationResult.raw.exp
     const actualExp = witness[1 + 2 * SHA256_BITS]
     assert.equal(expectedExp, actualExp)
 }
 
+/*
 describe("NZCP credential subject hash - example pass", function () {
     this.timeout(100000);
 
@@ -57,6 +63,38 @@ describe("NZCP credential subject hash - example pass", function () {
         console.log('calculating witness...');
 
         await testNZCPCredSubjHash(cir, EXAMPLE_PASS_URI, false, 314);
+    });
+});
+*/
+
+const LIVE_PASS_URI_1 = process.env.LIVE_PASS_URI_1;
+const LIVE_PASS_URI_2 = process.env.LIVE_PASS_URI_2;
+const LIVE_PASS_URI_3 = process.env.LIVE_PASS_URI_3;
+const LIVE_PASS_URI_4 = process.env.LIVE_PASS_URI_4;
+
+describe("NZCP credential subject hash - live pass", function () {
+    this.timeout(100000);
+
+    let cir
+    before(async () => {
+        cir = await wasm_tester(path.join(__dirname, "../circuits/nzcp_liveTest.circom"));
+    })
+
+    it ("Should generate credential hash and output exp for LIVE_PASS_URI_1", async () => {
+        console.log('calculating witness...');
+        await testNZCPCredSubjHash(cir, LIVE_PASS_URI_1, true, 355);
+    });
+    it ("Should generate credential hash and output exp for LIVE_PASS_URI_2", async () => {
+        console.log('calculating witness...');
+        await testNZCPCredSubjHash(cir, LIVE_PASS_URI_2, true, 355);
+    });
+    it ("Should generate credential hash and output exp for LIVE_PASS_URI_3", async () => {
+        console.log('calculating witness...');
+        await testNZCPCredSubjHash(cir, LIVE_PASS_URI_3, true, 355);
+    });
+    it ("Should generate credential hash and output exp for LIVE_PASS_URI_4", async () => {
+        console.log('calculating witness...');
+        await testNZCPCredSubjHash(cir, LIVE_PASS_URI_4, true, 355);
     });
 });
 
