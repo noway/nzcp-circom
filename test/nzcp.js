3a7872fa832f585f0d9b8bda429f5236b04c77b3
Ilia Sidorenko <ilia.sidorenko.421@gmail.com>
2022-02-20 22:36:49 +1300

    add testNZCPCredSubjHash function

--- test/nzcp.js
+++ test/nzcp.js
@@ -1,17 +1,15 @@
 const chai = require("chai");
 const path = require("path");
 const crypto = require("crypto");
-const assert = chai.assert;
-const { getToBeSignedAndRs } = require('./helpers/nzcp');
-const { verifyPassURIOffline, DID_DOCUMENTS } = require("@vaxxnz/nzcp");
-
-
 const wasm_tester = require("circom_tester").wasm;
+const { verifyPassURIOffline, DID_DOCUMENTS } = require("@vaxxnz/nzcp");
+const { getToBeSignedAndRs } = require('./helpers/nzcp');
+const { buffer2bitArray, bitArray2buffer } = require("./helpers/utils");
 
-const {buffer2bitArray, bitArray2buffer} = require("./helpers/utils");
+const assert = chai.assert;
 
-function prepareNZCPCredSubjHashInput(input) {
-    const maxLen = 314;
+
+function prepareNZCPCredSubjHashInput(input, maxLen) {
     const buffer = Buffer.alloc(maxLen).fill(0);
     input.copy(buffer, 0);
     return {toBeSigned: buffer2bitArray(buffer), toBeSignedLen: input.length}
@@ -22,6 +20,29 @@ function sha256Hash(input) {
 }
 
 const EXAMPLE_PASS_URI = "NZCP:/1/2KCEVIQEIVVWK6JNGEASNICZAEP2KALYDZSGSZB2O5SWEOTOPJRXALTDN53GSZBRHEXGQZLBNR2GQLTOPICRUYMBTIFAIGTUKBAAUYTWMOSGQQDDN5XHIZLYOSBHQJTIOR2HA4Z2F4XXO53XFZ3TGLTPOJTS6MRQGE4C6Y3SMVSGK3TUNFQWY4ZPOYYXQKTIOR2HA4Z2F4XW46TDOAXGG33WNFSDCOJONBSWC3DUNAXG46RPMNXW45DFPB2HGL3WGFTXMZLSONUW63TFGEXDALRQMR2HS4DFQJ2FMZLSNFTGSYLCNRSUG4TFMRSW45DJMFWG6UDVMJWGSY2DN53GSZCQMFZXG4LDOJSWIZLOORUWC3CTOVRGUZLDOSRWSZ3JOZSW4TTBNVSWISTBMNVWUZTBNVUWY6KOMFWWKZ2TOBQXE4TPO5RWI33CNIYTSNRQFUYDILJRGYDVAYFE6VGU4MCDGK7DHLLYWHVPUS2YIDJOA6Y524TD3AZRM263WTY2BE4DPKIF27WKF3UDNNVSVWRDYIYVJ65IRJJJ6Z25M2DO4YZLBHWFQGVQR5ZLIWEQJOZTS3IQ7JTNCFDX";
+
+async function testNZCPCredSubjHash(cir, passURI, isLive, maxLen) {
+    const verificationResult = verifyPassURIOffline(passURI, { didDocument: isLive ? DID_DOCUMENTS.MOH_LIVE : DID_DOCUMENTS.MOH_EXAMPLE })
+    const credSubj = verificationResult.credentialSubject;
+
+    const credSubjConcat = `${credSubj.givenName},${credSubj.familyName},${credSubj.dob}`
+    const toBeSignedByteArray = Buffer.from(getToBeSignedAndRs(passURI).ToBeSigned, "hex");
+
+    const input = prepareNZCPCredSubjHashInput(toBeSignedByteArray, maxLen);
+    const witness = await cir.calculateWitness(input, true);
+
+    const expectedCredSubjHash = sha256Hash(credSubjConcat)
+    const credSubjHash = bitArray2buffer(witness.slice(1, 257)).toString("hex");
+    assert.equal(credSubjHash, expectedCredSubjHash);
+
+    const expectedToBeSignedHash = sha256Hash(toBeSignedByteArray)
+    const toBeSignedHash = bitArray2buffer(witness.slice(257, 257+256)).toString("hex");
+    assert.equal(toBeSignedHash, expectedToBeSignedHash);
+
+    const expectedExp = verificationResult.raw.exp
+    const actualExp = witness[257+256]
+    assert.equal(expectedExp, actualExp)
+}
 
 describe("NZCP credential subject hash - example pass", function () {
     this.timeout(100000);
@@ -34,26 +55,7 @@ describe("NZCP credential subject hash - example pass", function () {
     it ("Should parse ToBeSigned", async () => {
         console.log('calculating witness...');
 
-        const verificationResult = verifyPassURIOffline(EXAMPLE_PASS_URI, { didDocument: DID_DOCUMENTS.MOH_EXAMPLE })
-        const credSubj = verificationResult.credentialSubject;
-    
-        const credSubjConcat = `${credSubj.givenName},${credSubj.familyName},${credSubj.dob}`
-        const toBeSignedByteArray = Buffer.from(getToBeSignedAndRs(EXAMPLE_PASS_URI).ToBeSigned, "hex");
-
-        const input = prepareNZCPCredSubjHashInput(toBeSignedByteArray);
-        const witness = await cir.calculateWitness(input, true);
-
-        const expectedCredSubjHash = sha256Hash(credSubjConcat)
-        const credSubjHash = bitArray2buffer(witness.slice(1, 257)).toString("hex");
-        assert.equal(credSubjHash, expectedCredSubjHash);
-
-        const expectedToBeSignedHash = sha256Hash(toBeSignedByteArray)
-        const toBeSignedHash = bitArray2buffer(witness.slice(257, 257+256)).toString("hex");
-        assert.equal(toBeSignedHash, expectedToBeSignedHash);
-
-        const expectedExp = verificationResult.raw.exp
-        const actualExp = witness[257+256]
-        assert.equal(expectedExp, actualExp)
+        await testNZCPCredSubjHash(cir, EXAMPLE_PASS_URI, false, 314);
     });
 });
 
