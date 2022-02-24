ee0e94b8d3d158d565217f5fea8f1628bb71a01e
Ilia Sidorenko <ilia.sidorenko.421@gmail.com>
2022-02-24 23:40:19 +1300

    add mixed str/int tests

--- test/cbor.js
+++ test/cbor.js
@@ -409,4 +409,18 @@ describe("CBOR SkipValue (array)", function () {
         const witness1 = await cir5.calculateWitness({ bytes, pos: 0 }, true);
         assert.equal(witness1[1], cbor.length);
     });
+    it ("SkipValue array of 2-byte string and 2-byte int", async () => {
+        const cbor = encodeArray([encodeStr('q'), encodeInt(0xFF)])
+        const bytes = padArray(cbor, MAX_LEN_5);
+        console.log(bytes)
+        const witness1 = await cir5.calculateWitness({ bytes, pos: 0 }, true);
+        assert.equal(witness1[1], cbor.length);
+    });
+    it ("SkipValue array of 2-byte string and 2 1-byte ints", async () => {
+        const cbor = encodeArray([encodeStr('q'), encodeInt(23), encodeInt(23)])
+        const bytes = padArray(cbor, MAX_LEN_5);
+        console.log(bytes)
+        const witness1 = await cir5.calculateWitness({ bytes, pos: 0 }, true);
+        assert.equal(witness1[1], cbor.length);
+    });
 });
